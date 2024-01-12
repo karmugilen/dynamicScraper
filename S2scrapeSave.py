712d79350923ac879b3e4e02d77b3f0c0bf49564
karmugilan <karmugil251@gmail.com>
2024-01-11 17:31:24 -0800

    testing

--- S2scrapeSave.py
+++ S2scrapeSave.py
@@ -1,10 +1,10 @@
 from autoscraper import AutoScraper
 
-url = 'https://www.amazon.in/Seiko-SRPD63K1-SEIKO-Sports-Auto/dp/B07Y53F3L4/ref=sr_1_18?crid=FV2QNJQYWW33&keywords=automatic+watch&qid=1704822266&sprefix=automatic+watch%2Caps%2C234&sr=8-18'
+url = 'https://www.amazon.in/Seiko-Analog-White-Dial-Watch-SUR312P1/dp/B084KP1GKP?ref_=ast_sto_dp'
 
 # We can add one or multiple candidates here.
 # You can also put urls here to retrieve urls.
-wanted_list = ["22,375"]
+wanted_list = ["₹20,586"]
 
 scraper = AutoScraper()
 result = scraper.build(url, wanted_list)
